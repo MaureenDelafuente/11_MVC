3366b697974837a9e4915fb584a4db6dae242cb9
Maureen <maureendelafuente.b@gmail.com>
2024-10-27 01:21:35 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Storage.Models;
+
+public class Product
+{// auto properties
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Price { get; set; }
+    public DateTime Orderdate { get; set; }
+    public required string Category { get; set; }
+    public required string Shelf { get; set; }
+    public int Count { get; set; }
+    public required string Description { get; set; }
+
+    
+}
